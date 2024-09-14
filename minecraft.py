eac0d63665ded2574c59a2b6ac0f20daf0114be7
Marcos Bião <dev.marcosdly@gmail.com>
2024-09-13 22:51:18 -0300

    feat: prevent download of already valid files

--- minecraft.py
+++ minecraft.py
@@ -2,10 +2,11 @@
 
 # ruff: noqa
 
-from typing import Never
+from typing import Never, TypeAlias
 
 import hashlib
 import os
+import pathlib
 import logging
 import random
 import time
@@ -14,6 +15,7 @@ import urllib.request as request
 import tomllib as toml
 
 
+MissingJars: TypeAlias = dict[str, list[str]]
 TIME_ONLY = "%H:%M:S"
 
 
@@ -23,18 +25,27 @@ def log(message):
     print(message)
 
 
-def download_jars(config: dict, key: str) -> Never | None:
+def download_jars(config: dict, key: str, missing: list[str] = []) -> Never | None:
     """
     :param config Parsed TOML config dictionary
     :param key    Key holding downloadable jar info (dict of dicts)
     """
     for basename, info in config[key].items():
         path = f"./{key}/{basename}.jar"
+
+        if len(missing) > 0 and basename not in missing:
+            log(f"File {path} doesn't need to be downloaded, skipping")
+            continue
+
         log(f"Downloading data for {basename}")
         start = datetime.now()
-        with request.urlopen(info["url"]) as jar_data, open(path, "wb") as jar_file:
+        with (
+            # request.urlopen(info["url"]) as jar_data,
+            open(path, "wb") as jar_file
+        ):
             finish = datetime.now()
-            data = jar_data.read()
+            # data = jar_data.read()
+            data = bytes()
             log(
                 f"Successfully downloaded data for {basename}, "
                 f"took {(finish - start).total_seconds()} seconds"
@@ -54,6 +65,41 @@ def download_jars(config: dict, key: str) -> Never | None:
         time.sleep(wait_time * 60)
 
 
+def check_jars(config: dict, keys: list[str]) -> MissingJars:
+    """Check files and hashes validity.
+    :param  config Parsed TOML config
+    :param  keys   Keys to check for downloadable jar info
+    :return List of key-value pair tuples that point to jars that must be redownloaded
+    """
+    must_redownload = {k: [] for k in keys}
+    for dir in keys:
+        jars = list(pathlib.Path(dir).glob("*.jar"))
+        config_keys = [jar.stem for jar in jars]
+        for key, path in zip(config_keys, jars):
+            if not os.path.exists(path):
+                log(f"File {path} doesn't exists. Flagging for download")
+                must_redownload[dir].append(key)
+                continue
+
+            with path.open("rb") as file:
+                data = file.read()
+                if len(data) == 0:
+                    log(f"File {path} exists but has no data. Flagging for download")
+                    must_redownload[dir].append(key)
+                    continue
+
+                local_file_hash = hashlib.sha256(data).hexdigest()
+                expected_hash = config[key]["hash"]
+                if local_file_hash != expected_hash:
+                    log(f"File {path} has an invalid hash. Flagging for download")
+                    must_redownload[dir].append(key)
+                    continue
+
+            log(f"File {path} is valid")
+
+    return must_redownload
+
+
 def prepare() -> None:
     logging.basicConfig(
         filename="./minecraft.log",
@@ -68,8 +114,10 @@ def prepare() -> None:
     jar_dirs = ["bin", "plugins"]
     for dirname in jar_dirs:
         os.makedirs(dirname, exist_ok=True)
+    to_download = check_jars(config, jar_dirs)
+    print(to_download)
     for dirname in jar_dirs:
-        download_jars(config, dirname)
+        download_jars(config, dirname, to_download[dirname])
 
 
 if __name__ == "__main__":
